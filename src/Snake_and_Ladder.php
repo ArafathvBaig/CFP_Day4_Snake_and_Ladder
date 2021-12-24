1bbf018f3912eb559040fae0fd57b945ec182289
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-24 11:53:28 +0530

    Added: File With UC1-Set start position of player to 0

--- src/Snake_and_Ladder.php
+++ src/Snake_and_Ladder.php
@@ -1,2 +1,5 @@
 <?php
-echo "Welcome to Snake and Ladder Computation Problem";
+echo "Welcome to Snake and Ladder Computation Problem\n";
+
+$startPosition = 0;
+echo "Start Position:: ".$startPosition;
